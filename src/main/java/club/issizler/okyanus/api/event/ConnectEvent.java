0d6c7db3a57b15d352b42cab9824eb2eb7d5e981
Ecmel Berk Canlıer <me@ecmelberk.com>
2019-06-29 13:49:18 +0300

    Add ConnectEvent.setCancelReason

--- src/main/java/club/issizler/okyanus/api/event/ConnectEvent.java
+++ src/main/java/club/issizler/okyanus/api/event/ConnectEvent.java
@@ -4,4 +4,13 @@ package club.issizler.okyanus.api.event;
  * This event fires whenever a player connects the server
  */
 public interface ConnectEvent extends PlayerEvent, Cancellable {
+
+    /**
+     * Sets the cancellation reason
+     *
+     * This will be the text shown to user when
+     * this event gets cancelled.
+     */
+    void setCancelReason(String reason);
+
 }
