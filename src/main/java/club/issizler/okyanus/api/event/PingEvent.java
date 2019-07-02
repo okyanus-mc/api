95e7baede3fec92febc9cb54db084312722007f4
Ecmel Berk Canlıer <me@ecmelberk.com>
2019-07-02 17:58:55 +0300

    Add server list ping event

---
+++ src/main/java/club/issizler/okyanus/api/event/PingEvent.java
@@ -0,0 +1,7 @@
+package club.issizler.okyanus.api.event;
+
+/**
+ * This event fires whenever a Minecraft client pings the server from the ingame server list
+ */
+public interface PingEvent extends Event {
+}
