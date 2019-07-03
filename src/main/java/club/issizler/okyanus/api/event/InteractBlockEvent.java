28b016385bc0e73b96e47d51842b886a9569517f
Ecmel Berk Canlıer <me@ecmelberk.com>
2019-07-03 18:42:59 +0300

    Improve InteractBlockEvent

--- src/main/java/club/issizler/okyanus/api/event/InteractBlockEvent.java
+++ src/main/java/club/issizler/okyanus/api/event/InteractBlockEvent.java
@@ -3,5 +3,5 @@ package club.issizler.okyanus.api.event;
 /**
  * This event fires whenever a block has been interacted by a player
  */
-public interface InteractBlockEvent extends PlayerEvent, Cancellable {
+public interface InteractBlockEvent extends PlayerEvent, Cancellable, Locatable {
 }
