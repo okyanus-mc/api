554bcee8b2f1c935c74282035812c65ed3763618
Ecmel Berk Canlıer <me@ecmelberk.com>
2019-07-04 13:42:15 +0300

    Add PlaceEvent.getBlock()

--- src/main/java/club/issizler/okyanus/api/event/PlaceEvent.java
+++ src/main/java/club/issizler/okyanus/api/event/PlaceEvent.java
@@ -1,7 +1,16 @@
 package club.issizler.okyanus.api.event;
 
+import club.issizler.okyanus.api.world.Block;
+
 /**
  * This event fires whenever a block has been placed by a player
  */
 public interface PlaceEvent extends PlayerEvent, Cancellable, Locatable {
+
+    /**
+     * Gets the block that has been placed
+     * @return the block that has been placed
+     */
+    Block getBlock();
+
 }
